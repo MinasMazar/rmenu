4715bee604eb5eec38d6c18c0cd8f8fa5a930035
MinasMazar <minasmazar@gmail.com>
2017-04-23 04:18:30 +0200

    Initial commit: bundle gem rmenu

---
+++ spec/rmenu_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Rmenu do
+  it "has a version number" do
+    expect(Rmenu::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
